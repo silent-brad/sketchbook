2265b8c2b0ff693c512e4f4cc1560050274c1798
bwhtdev <bradscottwhite@gmail.com>
2025-10-02 00:44:46 -0400

    made work

--- sketchbook.nimble
+++ sketchbook.nimble
@@ -1,14 +1,12 @@
 # Package
 
-version       = "0.1.0"
-author        = "Brad White"
-description   = "Digital Sketchbook"
-license       = "MIT"
-srcDir        = "src"
-bin           = @["sketchbook"]
-
+version = "0.1.0"
+author = "Brad White"
+description = "Digital Sketchbook"
+license = "MIT"
+srcDir = "src"
+bin = @["sketchbook"]
 
 # Dependencies
 
-requires "nim >= 2.0.16",
-         "nimja"
+requires "nim >= 2.0.16", "nimja"
--- src/sketchbook.nim
+++ src/sketchbook.nim
@@ -1,34 +1,64 @@
 import std/asynchttpserver, std/asyncdispatch
 import nimja/parser
-import os, random # Used in templates
+import std/sequtils, std/paths, std/strutils
+import os, random
 
-type Sketch = object
+type Sketch = object #title: string
   title: string
   file_name: string
-  tags: seq[string]
-  date: string
+  #tags: seq[string] # detect from subdir and only have 1
+  #date: string
 
 proc render_index(title: string, sketches: seq[Sketch]): string =
   compile_template_file("../templates/index.nimja", base_dir = get_script_dir())
 
+proc cb(req: Request) {.async.} =
+  var sketches: seq[Sketch] = @[]
+  for _, file in walk_dir("sketches/"):
+    let filename = file.split("/")[1]
+    sketches.add(
+      Sketch(title: filename.split(".")[0].replace("_", " "), file_name: filename)
+    )
+
+  let html = render_index("Brad's Sketchbook", sketches)
+  await req.respond(Http200, html)
+
+proc serve_static_file(req: Request, filename: string) {.async.} =
+  # exist -> have access -> can open
+  let path = "sketches/" / filename
+
+  # whether exists file
+  if not file_exists(path):
+    await req.respond(Http404, "File doesn't exist.", newHttpHeaders())
+
+  # whether has access to file
+  var filePermission = getFilePermissions(path)
+  if fpOthersRead notin filePermission:
+    await req.respond(Http403, "You have no access to the file.", newHttpHeaders())
+
+  # whether can open file
+  try:
+    let content = readFile(path)
+    await req.respond(Http200, content, newHttpHeaders())
+  except IOError:
+    await req.respond(Http404, "404 Not Found.", newHttpHeaders())
+
 proc main() {.async.} =
   var server = new_async_http_server()
 
-  proc cb(req: Request) {.async.} =
-    # Hardcoded data for the template (could come from a DB in a real app)
-    let sketches: seq[Sketch] =
-      @[
-        Sketch(
-          title: "Sketch 1",
-          file_name: "sketch.png",
-          tags: @["sketches"],
-          date: "2025-10-01",
-        )
-      ]
-    let html = render_index("Brad's Sketchbook", sketches)
-    await req.respond(Http200, html)
+  proc handler(req: Request) {.async.} =
+    var path = req.url.path
+    if path.split("/sketches/").len == 2:
+      await serve_static_file(req, path.split("/sketches/")[1])
 
-  server.listen(Port(8081))
+    case path
+    of "/":
+      await cb(req)
+    else:
+      await req.respond(Http404, "404 Not Found.", newHttpHeaders())
+
+  #server.listen(Port(8081))
+  wait_for server.serve(Port(8081), handler)
   echo "Server listening on http://localhost:8081/"
   while true:
     if server.should_accept_request():
